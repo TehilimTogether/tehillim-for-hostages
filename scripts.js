2da7158796a6cb270289736a2a98439c610c1bdc
TehilimTogether <tehilimtogether1@gmail.com>
2024-12-19 21:57:04 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 // Function to fetch hostage names from the Google Apps Script Web App
 const getHostages = async () => {
   try {
-    const response = await fetch("https://script.google.com/macros/s/AKfycbwbj1MLCTVPHDHrSwhHC5iuEtjMYc_SmACWn0ynrcX6L-nb9xJ3xcjiHo1OCrexZ8pK/exec");
+    const response = await fetch("https://script.google.com/macros/s/AKfycbyc7_7o8Y_AYw4l4oP41cBgFOKcNr_asBdlKp0H6KtYOyStTYiJLh2jVL-D5UfdB-w5/exec");
     if (!response.ok) throw new Error(`Network response was not OK. Status: ${response.status}`);
     const hostages = await response.json();
 
